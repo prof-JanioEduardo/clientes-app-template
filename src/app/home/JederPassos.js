2a0021a1b5bf24165f1de1413067bf15cf1a7bb0
JederPassos <jrpereirapassos@gmail.com>
2025-03-25 12:29:05 -0300

    Eu Jeder Passos acredito na virada!

---
+++ src/app/home/JederPassos.js
@@ -0,0 +1,11 @@
+class JederPassos {
+    constructor() {
+        
+        meu.nome = "Jeder Passos";
+    }
+        mostrarNome() {
+        console.log(meu.nome);
+    }
+}
+
+export default JederPassos;
